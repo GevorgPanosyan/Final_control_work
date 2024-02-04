4bb8d017626bc3c46d1ae4df933aaf0a37866645
GevorgPanosyan <gps79@yandex.ru>
2024-02-04 17:56:03 +0300

    Forming a new array of strings

--- Program.cs
+++ Program.cs
@@ -8,3 +8,7 @@ class Program
         Console.WriteLine("Введите элементы массива через запятую");
         string input = Console.ReadLine();
         string[]array = input.Split(',');
+
+        //Формируем новый массив из строк,длина которых <=3 символам
+        string[] resultArray = GetShortStrings(array);
+        
