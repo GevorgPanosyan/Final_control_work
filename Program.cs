ffb078052a8683baa1d4e41c73cd758dc2fa2926
GevorgPanosyan <gps79@yandex.ru>
2024-02-04 18:18:48 +0300

    Displaying a new array on the screen

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 class Program
 {
@@ -11,4 +12,33 @@ class Program
 
         //Формируем новый массив из строк,длина которых <=3 символам
         string[] resultArray = GetShortStrings(array);
-        
+
+        // Выводим новый массив на экран
+        Console.WriteLine("Новый массив:");
+        foreach (string item in resultArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    static string[] GetShortStrings(string[] array)
+    { 
+        int count = 0;
+        foreach (string item in array)
+        {
+            if (item.Lenght <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+        foreach (string item in array)
+        {
+            if (item.Lenght <=3)
+            {
+                resultArray[index] = item;
+                index++;
+            }
+        }
+    }
