362d1c14230fad6dc70a0c0f1525e25ae42c556c
GevorgPanosyan <gps79@yandex.ru>
2024-02-04 17:51:12 +0300

    Entering the source array from the keyboard

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+class Program
+{
+    static void Main()
+    {
+        //Вводим исходный массив с клавиатуры
+        Console.WriteLine("Введите элементы массива через запятую");
+        string input = Console.ReadLine();
+        string[]array = input.Split(',');
